48d94923c14320e91bd5e576a6870c2b187a5d22
Chad <cedie@hotmail.com>
2014-04-28 19:07:19 -0700

    cleaned up missing-type error handling.

--- lib/card.rb
+++ lib/card.rb
@@ -6,15 +6,10 @@ class Card
     @type     = params[:type]
     @username = params[:username]
     @password = params[:password]
-    begin
-      @klass  = Object.const_get(@type.capitalize)
-    rescue => e
-      puts e.message
-      puts "unknown class #{@type.capitalize}"
-    end
+    @klass    = Object.const_get(@type.capitalize) if Object.const_defined?(@type.capitalize)
   end
 
   def reward_balance
-    @klass.card_balance(@username, @password)
+    @klass.card_balance(@username, @password) unless @klass.nil?
   end
 end
--- lib/reward/united.rb
+++ lib/reward/united.rb
@@ -1,8 +1,6 @@
 class United < Reward
-  attr_accessor :type, :username, :password
-  
+
   def self.card_balance(username, password)
     puts "call to united card_balance."
-    puts "card_balance @username: #{self.inspect}"
   end
 end
